bca5d25c052f5bf111cbc5e1588f607988f08712
Jonathan Ward <j@w4rd.com>
2016-09-15 14:09:53 -0500

    cleanup

--- js/make.js
+++ js/make.js
@@ -152,7 +152,7 @@ else if((document.getElementById("code").value)=="sbp"){
    g=""
 
 	if(document.getElementById("units").value=="mm"){
-		g+="VD,4,1,1\n"
+		g+="VD,1,5,1 \n"
 	}
 
    g+="JZ,0.25\n"
@@ -162,29 +162,21 @@ else if((document.getElementById("code").value)=="sbp"){
    g+="PAUSE 3\n"
    g+="MS," + document.getElementById("feedrate").value + "," + document.getElementById("feedrate").value/2 + "\n"  //set feed,plunge
 
-for(i=0;i<dots.length;i++){
+	for(i=0;i<dots.length;i++){
 
    for(j=0;j<dots[i].length;j++){
            if(j==0){
-
-           g+="J2,"+(((dots[i][j].x-xmin)/sf2)/96).toFixed(3)+","+((((ymax-ymin)/sf2)+((ymin-(dots[i][j].y))/sf2))/96).toFixed(3) + "\n" 
-
-			  g+="MB,"+ (Math.atan2(dots[i][j+1].x-dots[i][j].x,dots[i][j+1].y-dots[i][j].y)*(180/Math.PI)).toFixed(3) +"\n"
-           g+="MZ" + (document.getElementById("depth").value)+ "\n"
-   		  g+="MI,Z,1,0,5,0.05\n"
-			  
-			  a = parseFloat((Math.atan2(dots[i][j+1].x-dots[i][j].x,dots[i][j+1].y-dots[i][j].y)*(180/Math.PI)).toFixed(3))
-
+	           g+="J2,"+(((dots[i][j].x-xmin)/sf2)/96).toFixed(3)+","+((((ymax-ymin)/sf2)+((ymin-(dots[i][j].y))/sf2))/96).toFixed(3) + "\n" 
+				  g+="MB,"+ (Math.atan2(dots[i][j+1].x-dots[i][j].x,dots[i][j+1].y-dots[i][j].y)*(180/Math.PI)).toFixed(3) +"\n"
+	           g+="MZ" + (document.getElementById("depth").value)+ "\n"
+	   		  g+="MI,Z,1,0,5,0.05\n"			  
+				  a = parseFloat((Math.atan2(dots[i][j+1].x-dots[i][j].x,dots[i][j+1].y-dots[i][j].y)*(180/Math.PI)).toFixed(3))
            }
            else{
 			  	if(j<dots[i].length-1){
-
 					b = a
-
 					a = parseFloat((Math.atan2(dots[i][j+1].x-dots[i][j].x,dots[i][j+1].y-dots[i][j].y)*(180/Math.PI)).toFixed(3))					
-
 					if((Math.abs(a-b))>(retract_angle)){
-
 						g+="MI,0\n"
 						g+="JZ,0.25\n"				
 						g+="MB,"+ a +"\n"
@@ -192,27 +184,24 @@ for(i=0;i<dots.length;i++){
    		 			g+="MI,Z,1,0,5,0.05\n"
 						g+="M2,"+(((dots[i][j].x-xmin)/sf2)/96).toFixed(3)+","+((((ymax-ymin)/sf2)+((ymin-(dots[i][j].y))/sf2))/96).toFixed(3) + "\n"
 					}
-
 					else{
 			  			g+="MB,"+ a +"\n"
 						g+="M2,"+(((dots[i][j].x-xmin)/sf2)/96).toFixed(3)+","+((((ymax-ymin)/sf2)+((ymin-(dots[i][j].y))/sf2))/96).toFixed(3) + "\n"
-					}
-					
+					}				
 			  	}
 			  	else{
-					g+="M2,"+(((dots[i][j].x-xmin)/sf2)/96).toFixed(3)+","+((((ymax-ymin)/sf2)+((ymin-(dots[i][j].y))/sf2))/96).toFixed(3) + "\n"
-					           
+					g+="M2,"+(((dots[i][j].x-xmin)/sf2)/96).toFixed(3)+","+((((ymax-ymin)/sf2)+((ymin-(dots[i][j].y))/sf2))/96).toFixed(3) + "\n"				           
 					}
            }
    }
 
-g+="MI,0\n"
-g+="JZ,0.25\n"
+	g+="MI,0\n"
+	g+="JZ,0.25\n"
 
-}
+	}
 
-g+="JB,0\n"
-g+="J3,0,0,0.25\n"
+	g+="JB,0\n"
+	g+="J3,0,0,0.25\n"
 
    fabmo.submitJob({
       file : g,
@@ -232,7 +221,7 @@ else if((document.getElementById("code").value)=="sbp2"){
 
 	if(document.getElementById("units").value=="mm"){
 		scale = 25.4
-		g+="VD,4,1,1,2\n"
+		g+="VD,1,5,1 \n"
 	}
 
    g+="JZ," + (0.25*scale).toFixed(3) + "\n"
@@ -259,26 +248,19 @@ for(i=0;i<saw.length;i++){
    for(j=1;j<saw[i].length-1;j++){
 
 		if(saw[i][j].r==true){
-//
 			if((document.getElementById("saw").value=="sawtooth") && (saw[i][j].z==(parseFloat(document.getElementById("depth").value)))){
-
 				g+="M3,"+((((saw[i][j].x-xmin)/sf2)/96)*scale).toFixed(3)+","+(((((ymax-ymin)/sf2)+((ymin-(saw[i][j].y))/sf2))/96)*scale).toFixed(3) + "," + (saw[i][j].z*scale).toFixed(3) + "\n"
-
 				g+="JZ," + ((parseFloat(document.getElementById("depth").value)*scale)+(parseFloat(document.getElementById("amplitude").value))*scale).toFixed(3) + "\n"
 			}
 
 			else if((document.getElementById("saw").value=="sawtooth") && (saw[i][j].z!=(parseFloat(document.getElementById("depth").value)))){
-
 				g+="M3,"+((((saw[i][j].x-xmin)/sf2)/96)*scale).toFixed(3)+","+(((((ymax-ymin)/sf2)+((ymin-(saw[i][j].y))/sf2))/96)*scale).toFixed(3) + "," + ((document.getElementById("depth").value)*scale).toFixed(3) + "\n"
-
 				g+="JZ," + ((parseFloat(document.getElementById("depth").value)*scale)+(parseFloat(document.getElementById("amplitude").value))*scale).toFixed(3) + "\n"
 			}
-
 			if((document.getElementById("offset").checked==true)&&(saw[i][j+1].r==true)){
 				g+="JZ," + (0.125*scale).toFixed(3) + "\n"
 				g+="JB," + (saw[i][j+1].a) + "\n"	
-				g+="J2," + ((((saw[i][j+1].x-xmin)/sf2)/96)*scale).toFixed(3)+","+(((((ymax-ymin)/sf2)+((ymin-(saw[i][j+1].y))/sf2))/96)*scale).toFixed(3) + "\n" 
-							
+				g+="J2," + ((((saw[i][j+1].x-xmin)/sf2)/96)*scale).toFixed(3)+","+(((((ymax-ymin)/sf2)+((ymin-(saw[i][j+1].y))/sf2))/96)*scale).toFixed(3) + "\n" 							
 			}
 			else if((document.getElementById("offset").checked==true)&&(saw[i][j+1].r==false)){
 
